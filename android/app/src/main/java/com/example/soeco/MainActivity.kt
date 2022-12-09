ab924e9c48b9fe08046cbb11277070c64d3f2113
Niklas Ling <72392907+nling43@users.noreply.github.com>
2022-12-09 21:44:44 +0100

    created project
    added colors
    added logos, todo => ask for better res on the white logo, ask for font
    created base xml

---
+++ android/app/src/main/java/com/example/soeco/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.soeco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       // setContentView(R.layout.base_small_title_layout)
+        setContentView(R.layout.base_layout)
+
+    }
+}
