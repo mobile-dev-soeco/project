219dcb594a4aedcbb62a0cc1db5c6a76252c3002
LAPTOP-7KGKGCAR\Sandr <sandrakaljula@hotmail.com>
2022-12-12 14:20:25 +0100

    Scrollable dashboard with order cards (not clickable)

--- android/app/src/main/java/com/example/soeco/MainActivity.kt
+++ android/app/src/main/java/com/example/soeco/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.soeco
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.soeco.dashboard.DashBoardFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // setContentView(R.layout.base_small_title_layout)
-        setContentView(R.layout.base_layout)
+        setContentView(R.layout.base_small_title_layout)
+        //setContentView(R.layout.base_layout)
 
+        val fragmentManager: FragmentManager = supportFragmentManager
+        var fragment = fragmentManager.findFragmentByTag("fragment_dash")
+        if (fragment == null) {
+            val ft: FragmentTransaction = fragmentManager.beginTransaction()
+            fragment = DashBoardFragment()
+            ft.add(R.id.card, fragment, "fragment_dash")
+            ft.commit()
+        }
     }
 }
