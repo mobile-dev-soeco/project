025b34711465f3fcf1cedf082b74930d4c125a28
Niklas Ling <72392907+nling43@users.noreply.github.com>
2022-12-29 17:49:01 +0100

    dev_nl2

--- android/app/src/main/java/com/example/soeco/ui/carpentry/CarpentryOrderDetailFragment.kt
+++ android/app/src/main/java/com/example/soeco/ui/carpentry/CarpentryOrderDetailFragment.kt
@@ -1,6 +1,4 @@
 package com.example.soeco.ui.carpentry
-
-import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,23 +6,16 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-
 import com.example.soeco.R
-import com.example.soeco.ui.viewmodels.DashBoardViewModel
-import com.example.soeco.ui.dashboard.DashBoardFragmentDirections
-import com.example.soeco.ui.viewmodels.OrderDetailsViewModel
-import com.example.soeco.utils.viewModelFactory
-import java.util.*
+
 
 class CarpentryOrderDetailFragment : Fragment() {
 
     val args: CarpentryOrderDetailFragmentArgs by navArgs()
     private val navigation: NavController by lazy { findNavController() }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
