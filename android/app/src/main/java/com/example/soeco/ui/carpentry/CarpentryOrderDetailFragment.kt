618703181d5256d78120f1f185bd959a86048b34
chinita226 <angel89226@live.cn>
2022-12-29 15:21:55 +0100

    reverse  carpentry order detail

--- android/app/src/main/java/com/example/soeco/ui/carpentry/CarpentryOrderDetailFragment.kt
+++ android/app/src/main/java/com/example/soeco/ui/carpentry/CarpentryOrderDetailFragment.kt
@@ -8,21 +8,17 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.soeco.R
-import com.example.soeco.ui.viewmodels.DashBoardViewModel
-import com.example.soeco.ui.viewmodels.OrderDetailsViewModel
-import com.example.soeco.utils.viewModelFactory
+
 import java.util.*
 
 class CarpentryOrderDetailFragment : Fragment() {
 
-    //val args: CarpentryOrderDetailFragmentArgs by navArgs()
+    val args: CarpentryOrderDetailFragmentArgs by navArgs()
 
-    private val dashBoardViewModel by viewModels<DashBoardViewModel> { viewModelFactory }
     private val navigation: NavController by lazy { findNavController() }
 
 
@@ -31,19 +27,14 @@ class CarpentryOrderDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_carpentry_order_detail, container, false)
-        val viewmodel by viewModels<OrderDetailsViewModel> { viewModelFactory }
 
         val orderNumberTextView : TextView = view.findViewById(R.id.textView_carpentry_products_view_order_number)
         val viewProductsButton : Button = view.findViewById(R.id.button_carpentry_viewProducts)
         val viewMaterialsButton : Button = view.findViewById(R.id.button_carpentry_viewMaterials)
         val reportDeviationButton: Button = view.findViewById(R.id.button_carpentry_reportDeviation)
-        val order_id = this.arguments?.getString("order")
-        //val order_id = args.orderNumber
-        orderNumberTextView.text= order_id.toString()
-//        val order = order_id.let { viewmodel.getOrder(it) }
-//        if (order != null) {
-//            orderNumberTextView.text= order.OrderNumber
-//        }
+
+        val order_id = args.orderNumber
+        orderNumberTextView.text= order_id
 
         viewProductsButton.setOnClickListener {
             navigation.navigate(R.id.action_carpentryOrderDetailFragment_to_productsList)
